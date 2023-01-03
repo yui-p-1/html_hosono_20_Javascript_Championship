9d764958a20c8a5f587e00e523636f4011df5b43
yui <hosono.yui@outlook.jp>
2023-01-03 16:05:54 +0900

    16:05 正常動作

--- js/script_5.js
+++ js/script_5.js
@@ -1,67 +1,48 @@
-// // 散布図データ
-// import mapdata from '../json/map.json' assert {type: 'json'};
-
-// const mapdataArr = Object.values(mapdata);
-// console.log(mapdataArr);
-
+const maArr = [{"COUNTRY_CODE":"AL","NAME_EN":"ALBANIA","CAPITAL_EN":"TIRANA","LAT":"41.3274082","LON":"19.8193172"},{"COUNTRY_CODE":"AG","NAME_EN":"ANTIGUA AND BARBUDA","CAPITAL_EN":"ST. JOHN'S","LAT":"17.1181446","LON":"-61.8339573"},{"COUNTRY_CODE":"BJ","NAME_EN":"BENIN","CAPITAL_EN":"PORTO-NOVO","LAT":"6.4726571","LON":"2.6420373"},{"COUNTRY_CODE":"BF","NAME_EN":"BURKINA FASO","CAPITAL_EN":"OUAGADOUGOU","LAT":"12.3680826","LON":"-1.527066"},{"COUNTRY_CODE":"BH","NAME_EN":"BAHRAIN","CAPITAL_EN":"MANAMA","LAT":"26.2334989","LON":"50.5720162"},{"COUNTRY_CODE":"BA","NAME_EN":"BOSNIA AND HERZEGOVINA","CAPITAL_EN":"SARAJEVO","LAT":"43.8589239","LON":"18.4334395"}]
+    
 var map = null, infobox, dataLayer;
+ function GetMap() 
+ {
+     map = new Microsoft.Maps.Map(document.getElementById("myMap"),
+       {
+           credentials: "Bing Mpas Key",
+           center: new Microsoft.Maps.Location(35, 140)});
+   dataLayer = new Microsoft.Maps.EntityCollection();
+   map.entities.push(dataLayer);
+   map.setView({ zoom: 1 });
+ var infoboxLayer = new Microsoft.Maps.EntityCollection();
+   map.entities.push(infoboxLayer);
+   infobox = new Microsoft.Maps.Infobox();
+   infoboxLayer.push(infobox);
+   AddData();
+ };
 
-  function GetMap() 
-  {
-      map = new Microsoft.Maps.Map(document.getElementById("myMap"),
-         {
-            credentials: "Bing Mpas Key",
-            center: new Microsoft.Maps.Location(0, 29)});
-     dataLayer = new Microsoft.Maps.EntityCollection();
-     map.entities.push(dataLayer);
-     map.setView({ zoom: 5 });
-   var infoboxLayer = new Microsoft.Maps.EntityCollection();
-     map.entities.push(infoboxLayer);
-     infobox = new Microsoft.Maps.Infobox();
-     infoboxLayer.push(infobox);
-     AddData();
-  };
+   pin = new Array(maArr.length)
+   function AddData() {
+   for (let i = 0; i < maArr.length; i++) {          
+     pin[i] = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(maArr[i].LAT, maArr[i].LON));
+     pin[i].Title = maArr[i].NAME_EN;
+     pin[i].Description = JSON.stringify(maArr[i]);
+     console.log(pin[i]);
+     Microsoft.Maps.Events.addHandler(pin[i], 'click', displayInfobox);
+     dataLayer.push(pin[i]);
+   };
+   };
 
-  // // チャレンジ
-  // let pin
-  // function AddData() {
-  //   for (let i = 0; i < mapdataArr.length; i++) {
-  //     var pin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(mapdataArr[i].LAT, mapdataArr[i].LON));
-  //     pin[i].Title = "This is + Pin[i]";
-  //     pin[i].Description = "Pin[i] + description";
-  //     Microsoft.Maps.Events.addHandler(pin[i], 'click', displayInfobox);
-  //     dataLayer.push(pin[i]);
-  //     };
-  //   };
+   console.log(maArr[0]);
 
-
-
-function AddData() {
-//Below is temp data that contains lat , lng and description values.
-    var pin1 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(0, 30));
-    pin1.Title = "This is Pin 1";
-    pin1.Description = "Pin 1 description";
-    Microsoft.Maps.Events.addHandler(pin1, 'click', displayInfobox);
-    dataLayer.push(pin1);
-
-    var pin2 = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(35.4779136, 139.6211712));
-    pin2.Title = "This is Pin 2";
-    pin2.Description = "Pin 2 description";
-    Microsoft.Maps.Events.addHandler(pin2, 'click', displayInfobox);
-    dataLayer.push(pin2);
-}
-
-function displayInfobox(e) {
-    if (e.targetType == 'pushpin') {
-        infobox.setLocation(e.target.getLocation());
+ function displayInfobox(e) {
+     if (e.targetType == 'pushpin') {
+         infobox.setLocation(e.target.getLocation());
                  
-        infobox.setOptions({
-            visible: true,
-            title: e.target.Title,
-            description: e.target.Description
-        });
-    }
-}
+         infobox.setOptions({
+             visible: true,
+             title: e.target.Title,
+             description: e.target.Description
+         });
+     }
+ }
+
 
 // Create Bing Maps with multiple pushpin and infobox
 // https://www.fourthbottle.com/2016/04/create-bing-maps-with-multiple-pushpin-infobox.html
